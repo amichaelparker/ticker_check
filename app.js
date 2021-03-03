4c1d9525d7d32ec946144dc2ddc89cb6113fafc5
Adam Parker <amichaelparker@gmail.com>
2021-03-02 23:07:29 -0600

    update ticker list, syntax changes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const fs = require('fs');
-const Excel = require('exceljs');
-const workbook = new Excel.Workbook();
-require('dotenv').config();
-const Snoowrap = require('snoowrap');
-require('snoostorm');
-const { CommentStream } = require("snoostorm");
+import fs from 'fs';
+import Excel from 'exceljs';
+import dotenv from 'dotenv';
+import Snoowrap from 'snoowrap';
+import { CommentStream } from 'snoostorm';
+import { tickers } from './tickers.js';
 
-const r = new Snoowrap({
+dotenv.config();
+const workbook = new Excel.Workbook();
+const client = new Snoowrap({
     userAgent: 'learning bot',
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
@@ -14,29 +15,23 @@ const r = new Snoowrap({
     password: process.env.REDDIT_PASS,
 });
 
-r.config({ requestDelay: 3000 })
-
-async function writeToExcel(comment, re, ignore) {
+const writeToExcel = (comment, re) => {
     if (comment.body.includes('$') && comment.body.match(re) !== null) {
         for (const item in comment.body.match(re)) {
+            if (!tickers.includes(comment.body.match(re)[item])) return;
             workbook.xlsx.readFile('tickers.xlsx').then(() => {
                 const worksheet = workbook.getWorksheet(1);
-                let written;
                 worksheet.eachRow({ includeEmpty: true }, (row) => {
-                    if (row.getCell(1).value === comment.body.match(re)[item] &&
-                        ignore.indexOf(comment.body.match(re)[item]) !== -1) {
+                    if (row.getCell(1).value === comment.body.match(re)[item]) {
                         row.getCell(2).value += 1;
                         row.commit();
-                        written = true;
+                    } else if (row.number === worksheet.rowCount) {
+                        let lastRow = worksheet.getRow(worksheet.rowCount + 1);
+                        lastRow.getCell(1).value = comment.body.match(re)[item];
+                        lastRow.getCell(2).value = 1;
+                        lastRow.commit();
                     }
                 });
-                if (!written && ignore.indexOf(comment.body.match(re)[item]) !== -1) {
-                    let row = worksheet.getRow(worksheet.rowCount + 1);
-                    row.getCell(1).value = comment.body.match(re)[item];
-                    row.getCell(2).value = 1;
-                    row.commit();
-                }
-                written = false;
                 return workbook.xlsx.writeFile('tickers.xlsx')
             }).then(() => console.log('Data Written'));
         }
@@ -63,37 +58,34 @@ const excelCheck = () => {
 }
 
 const tickerCheck = () => {
-    const re = /\$[A-Z]{1,4}/g;
-    const ignore = [
-        '$ANUS',
-        '$ROPE',
-        '$G',
-        '$J',
-        '$M',
-        '$P',
-        '$U',
-        '$FOMO',
-        '$APE',
-        '$DICK',
-        '$POOP',
-        '$COVD'
-    ];
-
     excelCheck();
+    const re = /\$[A-Z]{1,4}/g;
 
-    const streamOne = new CommentStream(r, { subreddit: "wallstreetbets", results: 25 });
-    streamOne.on("item", async (comment) => {
-        await writeToExcel(comment, re, ignore);
+    const streamOne = new CommentStream(client, {
+        subreddit: "wallstreetbets",
+        results: 25,
+        pollTime: 3000
+    });
+    streamOne.on("item", (comment) => {
+        writeToExcel(comment, re);
     })
 
-    const streamTwo = new CommentStream(r, { subreddit: "RobinHoodPennyStocks", results: 25 });
-    streamTwo.on("item", async (comment) => {
-        await writeToExcel(comment, re, ignore);
+    const streamTwo = new CommentStream(client, {
+        subreddit: "RobinHoodPennyStocks",
+        results: 25,
+        pollTime: 3000
+    });
+    streamTwo.on("item", (comment) => {
+        writeToExcel(comment, re);
     })
 
-    const streamThree = new CommentStream(r, { subreddit: "PennyStocks", results: 25 });
-    streamThree.on("item", async (comment) => {
-        await writeToExcel(comment, re, ignore);
+    const streamThree = new CommentStream(client, {
+        subreddit: "PennyStocks",
+        results: 25,
+        pollTime: 3000
+    });
+    streamThree.on("item", (comment) => {
+        writeToExcel(comment, re);
     })
 }
 
